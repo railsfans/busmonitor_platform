34fce3a80d00e393032a132e96a3b07bbb4a51cf
yxx <yxiangxiao@163.com>
2015-02-03 17:57:47 +0800

    fix config file

--- app/worker/hard_worker.rb
+++ app/worker/hard_worker.rb
@@ -7,7 +7,7 @@ class HardWorker
 		if !City.find(string).name.nil? && !City.find(string).ip.nil? 
 			mkcmd="mkdir -p /opt/database/"+City.find(string).name
 			system(mkcmd)
-			cmd="mysqldump -u root -proot -h "+City.find(string).ip+" materialsys_development>"+File.join('/opt/database',City.find(string).name+'/development.sql')
+			cmd="mysqldump -u root -proot -h "+City.find(string).ip+" wiads_development>"+File.join('/opt/database',City.find(string).name+'/development.sql')
 			status=system(cmd)
 			City.find(string).update_attributes(:backup_status=>2, :backup_time=>Time.now.to_s.split('+')[0]) if  type=='backup' && status==true
 			City.find(string).update_attributes(:backup_status=>3) if  type=='backup' && status==false
